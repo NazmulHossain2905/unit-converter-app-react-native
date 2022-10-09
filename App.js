a2d96725c7806385367a93e99d9527a62a91330d
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2022-10-09 10:57:35 +0600

    unit converter app complete

--- App.js
+++ App.js
@@ -1,117 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App() {
+  const [feet, setFeet] = React.useState('1');
+  const [inches, setInches] = React.useState('12');
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // for inches converter
+  const feetHandler = feet => {
+    setFeet(feet);
+    setInches(String(feet * 12));
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  // for feet converter
+  const inchesHandler = inches => {
+    let f = inches / 12;
+    setInches(inches);
+    setFeet(Number.isInteger(f) ? String(f) : String(f.toFixed(2)));
+  };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  // for feet converter
+  // const inchesHandler = inches => {
+  //   setInches(inches);
+  //   setFeet(
+  //     Number.isInteger(inches / 12)
+  //       ? String(inches / 12)
+  //       : String((inches / 12).toFixed(2)),
+  //   );
+  // };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Unit Converter App</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Feet</Text>
+        <TextInput
+          style={styles.input}
+          value={feet}
+          onChangeText={feetHandler}
+        />
+        <Text style={styles.label}>Inches</Text>
+        <TextInput
+          style={styles.input}
+          value={inches}
+          onChangeText={inchesHandler}
+        />
+      </View>
+    </View>
+  );
+}
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2ffff',
+    paddingTop: 40,
+    paddingHorizontal: 30,
+  },
+  text: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+  inputContainer: {
+    marginTop: 30,
+    backgroundColor: '#49dcdf',
+    padding: 15,
+    elevation: 10,
+  },
+  label: {
+    fontSize: 18,
+    color: '#222',
+    fontWeight: '600',
+  },
+  input: {
+    width: '100%',
+    backgroundColor: '#f2ffff',
+    borderWidth: 1,
+    borderColor: '#fff',
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    marginBottom: 16,
+    marginTop: 5,
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
